f6de129f32913a295c5b82f943c9e8096d60af70
m-holovan <nikita2003n@gmail.com>
2023-02-08 20:51:43 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using food_store.Models;
+using System.Diagnostics;
+
+namespace food_store.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success");
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+    }
+}
---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace food_store.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Enter Name")]
+        [StringLength(30, MinimumLength = 2)]
+        public string userName { get; set; }
+        [Required(ErrorMessage = "Enter E-mail")]
+        [EmailAddress]
+        [UIHint("EmailAddress")]
+        public string userEmail { get; set; }
+        [Required(ErrorMessage = "Enter Date")]
+        [UIHint("Date")]
+        public string userDate { get; set; }
+        [Required(ErrorMessage = "Enter Time")]
+        [UIHint("Time")]
+        public string userTime { get; set; }
+        [Required(ErrorMessage = "Enter Value")]
+        public string peopleValue { get; set; }
+    }
+}
